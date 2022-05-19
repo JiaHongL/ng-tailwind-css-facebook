f5c43f768e5e070de4ee41c396e1b4ef8e6286c0
joe <>
2022-05-19 20:47:49 +0800

    練習版面布局

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        'main-span': '72px'
+      },
+      colors: {
+        'fb-bg': '#18191a',
+        'fb-header': '#242526',
+        'fb-input': '#3a3b3c',
+        'fb-popover': '#3e4042',
+        'fb-active': '#323436',
+        'fb-card': '#242526',
+        'fb': '#2e89ff'
+      }
+    },
+  },
+  plugins: [],
+}
